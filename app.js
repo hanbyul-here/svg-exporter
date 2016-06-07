d20e855d3117e6da853536d50062ba1ea6a09360
Hanbyul Jo <hanbyul@nyu.edu>
2016-06-07 17:04:04 -0400

    fixing :/

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
